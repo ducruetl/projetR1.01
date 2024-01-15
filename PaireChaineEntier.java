09b673befc57703ae9893266012e75b8d7f20e78
balded1ams <153721259+balded1ams@users.noreply.github.com>
2024-01-15 10:39:25 +0100

    Add files via upload

--- PaireChaineEntier.java
+++ PaireChaineEntier.java
@@ -1,3 +1,13 @@
 public class PaireChaineEntier {
+    private String chaine;
+    private int entier;
 
+    // constructeur:
+    public PaireChaineEntier(String chaine, int entier) {
+        this.chaine = chaine;
+        this.entier = entier;
+    }
+
+    public String getChaine(){return chaine;}
+    public int getentier(){return entier;}
 }
