f1cd695b980a8dd310fb83736f87ee0eab717c6f
ducruetl <153935882+ducruetl@users.noreply.github.com>
2024-01-17 22:02:01 +0100

    Add files via upload

--- UtilitairePaireChaineEntier.java
+++ UtilitairePaireChaineEntier.java
@@ -4,21 +4,48 @@ public class UtilitairePaireChaineEntier {
 
 
     public static int indicePourChaine(ArrayList<PaireChaineEntier> listePaires, String chaine) {
-        return 0;
-
+        int cpt = 0;
+        for(PaireChaineEntier p : listePaires){
+            if(p.getChaine().equals(chaine)){
+                break;
+            }
+            cpt++;
+        }
+        if(cpt == listePaires.size()){
+            return -1;
+        }
+        return cpt;
     }
 
     public static int entierPourChaine(ArrayList<PaireChaineEntier> listePaires, String chaine) {
-        return 0;
+        int x = 0;
+        for(PaireChaineEntier e : listePaires){
+            if(e.getChaine().equals(chaine)){
+                x = e.getentier();
+            }
+        }
+
+        return x;
     }
 
     public static String chaineMax(ArrayList<PaireChaineEntier> listePaires) {
-        return "SPORTS";
+        PaireChaineEntier max = listePaires.get(0);     // la plaus grande valeur etant trois un parcour en entier n'est pas forcement necessaire.
+        for(PaireChaineEntier e: listePaires){
+            if(e.getentier()>max.getentier()){
+                max = e;
+            }
+        }
+        return max.getChaine();
     }
 
 
     public static float moyenne(ArrayList<PaireChaineEntier> listePaires) {
-        return 0;
+        int x = 0;
+        for(PaireChaineEntier e : listePaires){
+            x += e.getentier();
+        }
+
+        return x/listePaires.size();
     }
 
 }
